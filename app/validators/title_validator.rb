edf28e49e208b99ab2b9c86dd418d263ee5702a5
leighad <cdversiontwo@gmail.com>
2020-04-20 11:05:17 -0700

    Done.

---
+++ app/validators/title_validator.rb
@@ -0,0 +1,7 @@
+# class TitleValidator < ActiveModel::Validator 
+#     def validate(record)
+#         unless record.title.match? (%w(?))
+#     end
+# end
+
+#I guess you don't need this file for this particular lab....
