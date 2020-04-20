45eeeaaf98c3c3d6e89eda8a2ed20662126affd2
jonschacter <schacter.jon@gmail.com>
2020-04-20 12:52:28 -0700

    Done.

--- app/validators/post_validator.rb
+++
@@ -1,9 +0,0 @@
-class PostValidator < ActiveModel::Validator
-    def validate(record)
-        if !record.title.nil?
-            unless record.title.include?("You Won't" || "Secret" || "Top" || "Guess")
-                record.errors[:title] << "That is not a clickbait title!"
-            end
-        end
-    end
-end
