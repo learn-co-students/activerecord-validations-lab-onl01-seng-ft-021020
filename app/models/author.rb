d8e9c694ad8d574eb50ea4af786d90d6f616bdb9
taylorkraft <taylorkraft16@gmail.com>
2020-04-21 19:49:14 -0600

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,4 @@
 class Author < ActiveRecord::Base
-  include ActiveModel::Validations
-  validates_with UniquenessValidator
-  validates :name, presence: :true
-  validates :name, uniquness: true
+  validates :name, presence: true, uniqueness: true
   validates :phone_number, length: {is: 10}
 end
--- app/validators/uniqueness_validator.rb
+++
@@ -1,8 +0,0 @@
-class UniquenessValidator < ActiveModel::Validator
- 
-  def validate(record)
-    unless record.name.unique?
-      record.errors[:name] << "Someone has that name"
-    end
-  end
-end
